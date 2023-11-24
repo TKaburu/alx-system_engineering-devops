aac6eb79ebd23bcea478676935cba4aad125fbef
TKaburu <traceymwendwa@gmail.com>
2023-11-24 03:02:56 -0800

    Zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+/**
+ * infinite_while - This function creates an infinite loop
+ *
+ * Return: 0 if successful
+ */
+
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - This is the entry point of the program
+ *
+ * Return: 0 if successful
+ */
+
+
+int main(void)
+{
+	int zombies = 0;
+	pid_t babe;
+
+	while (zombies < 6)
+	{
+		babe = fork();
+		if (babe == 0)
+		{
+			printf("Zombie process created, PID: %d\n", babe);
+			exit(0);
+		}
+		zombies++;
+	}
+	infinite_while();
+	return (0);
+}
+
