4e9d55126397a414b4b1175c63617581a82a57a8
TKaburu <traceymwendwa@gmail.com>
2024-01-18 22:37:34 -0800

    Feat: Create a file using Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in /tmp with content I love Puppet, permision 0744, owner www-data, group www-data
+
+file { '/tmp/school':
+  ensure  => 'file',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+}
