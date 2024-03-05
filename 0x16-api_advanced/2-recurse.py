c32a450f082384d556883bc62ccae855cd734257
TKaburu <traceymwendwa@gmail.com>
2024-03-05 13:41:07 -0800

    Fix: Pycodestyle

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -11,16 +11,17 @@ def recurse(subreddit, hot_list=[]):
     for a given subreddit
     """
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+
     headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-            }
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
     params = {
             "after": after,
             "count": count,
             "limit": 100
             }
     response = requests.get(url, headers=headers, params=params,
-            allow_redirects=False)
+                            allow_redirects=False)
 
     if response.status_code == 404:
         return None
